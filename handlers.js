4369664226e77d00d7342fc487aee235fb2a1ada
Bimal Kumar <40321@hexaware.com>
2018-10-19 11:22:01 +0530

    aa

--- handlers.js
+++ handlers.js
@@ -5,4 +5,8 @@ alexaApp.launch(function (req, res) {
     res.say("Hey there, I’m Uni, your course adviser in Monash University. You can ask your questions like, What courses can you offer me?").shouldEndSession(false);
 });
 
+alexaApp.intent('StartConvIntent', function (req, res) {
+    res.say("You mean, the courses available for you to join?").shouldEndSession(false);
+});
+
 module.exports = alexaApp;
