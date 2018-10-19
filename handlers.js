62bd0ae2f1544464d6a7ca761f740fbfcdf004b3
Bimal Kumar <40321@hexaware.com>
2018-10-19 12:36:24 +0530

    s

--- handlers.js
+++ handlers.js
@@ -21,6 +21,7 @@ alexaApp.intent('YesIntent', function (req, res) {
     if (StartConvIntent) {
         askforQualification = true;
         res.say("Sure. What is your qualification?").shouldEndSession(false);
+        return res.send();
     }
     res.say(fallbackIntents[Math.floor(Math.random() * fallbackIntents.length)]).shouldEndSession(false);
 });
@@ -29,6 +30,7 @@ alexaApp.intent('QualificationIntent', function (req, res) {
     if (askforQualification) {
         askForAge = true;
         res.say("Good! How old are you, may I know? Trust me! I will forget it!").shouldEndSession(false);
+        return res.send();
     }
     res.say(fallbackIntents[Math.floor(Math.random() * fallbackIntents.length)]).shouldEndSession(false);
 });
@@ -37,6 +39,7 @@ alexaApp.intent('AgeIntent', function (req, res) {
     if (askForAge) {
         askForAspiration = true;
         res.say("Your age is eligible for a masters. Can you tell me your aspiration?").shouldEndSession(false);
+        return res.send();
     }
     res.say(fallbackIntents[Math.floor(Math.random() * fallbackIntents.length)]).shouldEndSession(false);
 });
@@ -44,6 +47,7 @@ alexaApp.intent('AgeIntent', function (req, res) {
 alexaApp.intent('NotSureIntent', function (req, res) {
     if (askForAspiration) {
         res.say("What is that you want to become?").shouldEndSession(false);
+        return res.send();
     }
     res.say(fallbackIntents[Math.floor(Math.random() * fallbackIntents.length)]).shouldEndSession(false);
 });
@@ -51,6 +55,7 @@ alexaApp.intent('NotSureIntent', function (req, res) {
 alexaApp.intent('AspirationIntent', function (req, res) {
     if (askForAspiration) {
         res.say("Great! What subjects did you like the most as a kid?").shouldEndSession(false);
+        return res.send();
     }
     res.say(fallbackIntents[Math.floor(Math.random() * fallbackIntents.length)]).shouldEndSession(false);
 });
