e56effe12d8c722fb55693fa9ce419518253ab97
Bimal Kumar <40321@hexaware.com>
2018-10-22 11:00:43 +0530

    dd

--- handlers.js
+++ handlers.js
@@ -17,15 +17,18 @@ askForOtherCourses = false;
 askIfAnythingElse = false;
 
 alexaApp.launch(function (req, res) {
+    console.log("App launched");
     res.say("Hey there, I’m Uni, your course adviser in Monash University. You can ask your questions like, What courses can you offer me?").shouldEndSession(false);
 });
 
 alexaApp.intent('StartConvIntent', function (req, res) {
+    console.log("Inside StartConvIntent");
     StartConvIntent = true;
     res.say("You mean, the courses available for you to join?").shouldEndSession(false);
 });
 
 alexaApp.intent('YesIntent', function (req, res) {
+    console.log("Inside YesIntent");
     if (StartConvIntent) {
         askforQualification = true;
         res.say("Sure. What is your qualification?").shouldEndSession(false);
@@ -35,6 +38,7 @@ alexaApp.intent('YesIntent', function (req, res) {
 });
 
 alexaApp.intent('QualificationIntent', function (req, res) {
+    console.log("Inside QualificationIntent");
     if (askforQualification) {
         askForAspiration = true;
         res.say("Good! Can you tell me your aspiration, like what do you want to become?").shouldEndSession(false);
@@ -44,6 +48,7 @@ alexaApp.intent('QualificationIntent', function (req, res) {
 });
 
 alexaApp.intent('AspirationIntent', function (req, res) {
+    console.log("Inside AspirationIntent");
     if (askForAspiration) {
         AspirationIntent = true;
         res.say("Great! What subjects did you like the most as a kid?").shouldEndSession(false);
@@ -53,6 +58,7 @@ alexaApp.intent('AspirationIntent', function (req, res) {
 });
 
 alexaApp.intent('SubjectsIntent', function (req, res) {
+    console.log("Inside SubjectsIntent");
     if (AspirationIntent) {
         SubjectsIntent = true;
         res.say("That’s in line with an Astronaut. What’s your grade in bachelor of physics?").shouldEndSession(false);
@@ -61,7 +67,8 @@ alexaApp.intent('SubjectsIntent', function (req, res) {
     res.say(fallbackIntents[Math.floor(Math.random() * fallbackIntents.length)]).shouldEndSession(false);
 });
 
-alexaApp.intent('PercentageIntent', function (req, res) {
+alexaApp.intent('PercentageIntent', function (req, res) {.
+    console.log("Inside PercentageIntent");
     if (SubjectsIntent) {
         PercentageIntent = true;
         elaborateAstronautSkills = true;
@@ -72,6 +79,7 @@ alexaApp.intent('PercentageIntent', function (req, res) {
 });
 
 alexaApp.intent('NotSureIntent', function (req, res) {
+    console.log("Inside NotSureIntent");
     if (elaborateAstronautSkills) {
         askAboutSwimming = true;
         res.say("Sure. Do you like swimming? Most part of your astronaut training is going to be under water.");
@@ -81,6 +89,7 @@ alexaApp.intent('NotSureIntent', function (req, res) {
 });
 
 alexaApp.intent('SwimUnderwaterIntent', function (req, res) {
+    console.log("Inside SwimUnderwaterIntent");
     if (askAboutSwimming) {
         askAboutSuperAdventureRides = true;
         res.say("How about riding a roller coaster or other super adventure rides?");
@@ -90,6 +99,7 @@ alexaApp.intent('SwimUnderwaterIntent', function (req, res) {
 });
 
 alexaApp.intent('HateIntent', function (req, res) {
+    console.log("Inside HateIntent");
     if (askAboutSuperAdventureRides) {
         askABoutSpaceResearch = true;
         res.say("Are you interested in observing and researching deep space?");
@@ -99,6 +109,7 @@ alexaApp.intent('HateIntent', function (req, res) {
 });
 
 alexaApp.intent('LoveIntent', function (req, res) {
+    console.log("Inside LoveIntent");
     if (askABoutSpaceResearch) {
         askForOtherCourses = true;
         res.say("Great! I think you may want to reconsider being an astronaut. You can still become a good scientist in space research by taking up Master in Astronomy and Astrophysics. Do you have a question?");
@@ -108,6 +119,7 @@ alexaApp.intent('LoveIntent', function (req, res) {
 });
 
 alexaApp.intent('LoveIntent', function (req, res) {
+    console.log("Inside LoveIntent");
     if (askForOtherCourses) {
         askIfAnythingElse = true;
         res.say("This program opens you up to positions with space and defense companies like Lockheed Martin, Northrup Gruman, Boeing, and National Space Agencies. I’ve sent you more details to your email. Is there anything else you’d like to know?");
@@ -117,6 +129,7 @@ alexaApp.intent('LoveIntent', function (req, res) {
 });
 
 alexaApp.intent('ThankIntent', function (req, res) {
+    console.log("Inside ThankIntent");
     YesIntent = false;
     StartConvIntent = false;
     askforQualification = false;
